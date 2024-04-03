1f355749ee80f53fc0ceaad218f942788ae9e162
gabo-a0179 <gabrilo0179@gmail.com>
2024-04-03 16:36:38 -0600

    Agregando ejemplo de programa que utiliza clases separado por archivos .cpp y .hpp; mainRO.cpp, personaRO.cpp y personaRO.hpp
    Signed-off-by: gabo-a0179 <gabrilo0179@gmail.com>

---
+++ Previos/Previo4/mainRO.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "personaRO.hpp"
+
+using namespace std;
+
+int main() {
+    Persona p("Juan", 25);
+
+    cout << "Nombre: " << p.getNombre() << endl;
+    cout << "Edad: " << p.getEdad() << endl;
+
+    p.setEdad(26);
+
+    cout << "Nueva edad: " << p.getEdad() << endl;
+
+    return 0;
+}
---
+++ Previos/Previo4/personaRO.cpp
@@ -0,0 +1,20 @@
+#include "personaRO.hpp"
+
+using namespace std;
+
+Persona::Persona(string nombre, int edad) {
+    this->nombre = nombre;
+    this->edad = edad;
+}
+
+string Persona::getNombre() {
+    return nombre;
+}
+
+int Persona::getEdad() {
+    return edad;
+}
+
+void Persona::setEdad(int edad) {
+    this->edad = edad;
+}
