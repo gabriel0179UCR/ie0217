cc765b7f60fe58801ede3ea8bd088029dd4eeed8
gabo-a0179 <gabrilo0179@gmail.com>
2024-04-03 17:10:18 -0600

    Agregando ejemplo de herencia de clases en C++, inheritance.cpp
    Signed-off-by: gabo-a0179 <gabrilo0179@gmail.com>

---
+++ Previos/Previo4/inheritance.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+    public:
+        void eat() {
+            cout << "I can eat!" << endl;
+        }
+
+        void sleep() {
+            cout << "I can sleep!" << endl;
+        }
+};
+
+class Dog : public Animal {
+    public:
+        void bark() {
+            cout << "I can bark! Woof woof!!" << endl;
+        }
+};
+
+int main() {
+    Dog dog1;
+
+    // Calling members of the base class
+    dog1.eat();
+    dog1.sleep();
+
+    // Calling member of the derived class
+    dog1.bark();
+
+    return 0;
+}
