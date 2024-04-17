86157458dd85fe3e4e6a7405ca1258ff164f7669
gabo-a0179 <gabrilo0179@gmail.com>
2024-04-16 18:37:32 -0600

    Agregando definicion de la estructura de los contactos
    Signed-off-by: gabo-a0179 <gabrilo0179@gmail.com>

---
+++ Tareas/Tarea3/Contacto.hpp
@@ -0,0 +1,14 @@
+#ifndef CONTACTO_HPP
+#define CONTACTO_HPP
+
+#include <iostream>
+#include <string>
+
+/* Estructura para definir los Contactos (Nombre y Telefono) */
+struct Contacto {
+    std::string nombre;
+    std::string telefono;
+};
+
+
+#endif // CONTACTO_HPP
