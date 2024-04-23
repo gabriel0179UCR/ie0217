1e05e05b829863e6d8817d18a201b04477b5569f
gabo-a0179 <gabrilo0179@gmail.com>
2024-04-22 20:18:13 -0600

    Agregando ejemplo de adaptadores de contenedores
    Signed-off-by: gabo-a0179 <gabrilo0179@gmail.com>

---
+++ Previos/Previo7/adaptersContainers.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    // Create a stack of ints
+    stack<int> numbers;
+
+    // push into stack
+    numbers.push(1);
+    numbers.push(100);
+    numbers.push(10);
+
+    cout << "Numbers are: ";
+
+    // we can access the element by getting the top and pooping
+    // until the stach is empty
+    while(!numbers.empty()) {
+        // print top element
+        cout << numbers.top() << ", ";
+
+        // pop top element from stack
+        numbers.pop();
+    };
+
+    return 0;
+}
