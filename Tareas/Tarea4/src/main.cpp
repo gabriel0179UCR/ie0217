a8108dc410663f11d7363aca0698d87fd8cb1b2d
Gabriel Angulo <gabrilo0179@gmail.com>
2024-05-07 19:07:01 -0600

    Agregando plantilla de la clase Matriz sin las definiciones
    Signed-off-by: Gabriel Angulo <gabrilo0179@gmail.com>

---
+++ Tareas/Tarea4/src/main.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+using namespace std;
+
+// Clase Matriz
+template <typename T>
+class Matriz {
+    public:
+
+        Matriz(int _filas, int _columnas) : filas(_filas), columnas(_columnas) {}
+        ~Matriz() {}
+        void setDimensiones(int filas, int columnas) {
+            return;
+        }
+        void llenarMatriz() {
+            return;
+        }
+    private:
+        vector<vector<T>> matriz;
+};
+
+// Clase OperacionesBasicas
+
+
+// Clase ValidadorDeEntrada
+
+
+// Menu
