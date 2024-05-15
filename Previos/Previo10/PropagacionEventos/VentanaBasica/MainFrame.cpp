e8b21035d801427631e56a02b0ae59812bbc060d
Gabriel Angulo <gabrilo0179@gmail.com>
2024-05-14 19:12:46 -0600

    Agregando ejemplo de propagacion de eventos con wxwidgets
    Signed-off-by: Gabriel Angulo <gabrilo0179@gmail.com>

---
+++ Previos/Previo10/PropagacionEventos/VentanaBasica/MainFrame.cpp
@@ -0,0 +1,34 @@
+#include "MainFrame.hpp"
+#include <wx/wx.h>
+
+MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
+	wxPanel* panel = new wxPanel(this);
+
+	wxButton* button1 = new wxButton(panel, wxID_ANY, "Button 1", wxPoint(300, 275), wxSize(200, 50));
+	wxButton* button2 = new wxButton(panel, wxID_ANY, "Button 2", wxPoint(300, 350), wxSize(200, 50));
+
+	this->Bind(wxEVT_CLOSE_WINDOW, &MainFrame::OnClose, this);
+	this->Bind(wxEVT_BUTTON, &MainFrame::OnAnyButtonClicked, this);
+	button1->Bind(wxEVT_BUTTON, &MainFrame::OnButton1Clicked, this);
+	button2->Bind(wxEVT_BUTTON, &MainFrame::OnButton2Clicked, this);
+
+	CreateStatusBar();
+}
+
+void MainFrame::OnAnyButtonClicked(wxCommandEvent& evt) {
+	wxLogStatus("Button Clicked");
+}
+
+void MainFrame::OnButton1Clicked(wxCommandEvent& evt) {
+	wxLogStatus("Button 1 Clicked");
+	evt.Skip(); // Continue la prograpacion del evento
+}
+
+void MainFrame::OnButton2Clicked(wxCommandEvent& evt) {
+	wxLogStatus("Button 2 Clicked");
+}
+
+void MainFrame::OnClose(wxCloseEvent& evt) {
+	wxLogMessage("Frame Close");
+	evt.Skip(); // Sin esto no se cerraria la ventana
+}
