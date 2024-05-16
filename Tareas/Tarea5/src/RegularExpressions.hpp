2741f733c5a06ecd8bdbf21498268c091aeebfc2
Gabriel Angulo <gabrilo0179@gmail.com>
2024-05-16 06:45:20 -0600

    Correccion a expresion regular
    Signed-off-by: Gabriel Angulo <gabrilo0179@gmail.com>

--- Tareas/Tarea5/src/RegularExpressions.hpp
+++ Tareas/Tarea5/src/RegularExpressions.hpp
@@ -9,7 +9,7 @@ std::string COUNT_CHARACTERS_NAME = "^[^\\.-]+$";
 std::string NOT_CONSECUTIVE_SPECIAL_CHARACTERS = "^[^a-zA-Z0-9]{2}+$";
 
 std::string ONLY_LETTERS = "^[^a-zA-Z\\.]+$";
-std::string POINT_NUMBER = "^([^.]|\\.{2,})+$";
+std::string POINT_NUMBER = "^[^.]+$";
 std::string COUNT_CHARACTERS_DOMAIN = "^[^\\.]+$";
 std::string NOT_CONSECUTIVE_POINTS = "\\.{2,}";
 
