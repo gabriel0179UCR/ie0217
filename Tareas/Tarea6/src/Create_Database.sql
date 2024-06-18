5d8053253ea01873a63a9cc814409a2de461d01b
Gabriel Angulo <gabrilo0179@gmail.com>
2024-06-18 16:44:48 -0600

    Codigo de SQL que crea la base de datos
    Signed-off-by: Gabriel Angulo <gabrilo0179@gmail.com>

---
+++ Tareas/Tarea6/src/Create_Database.sql
@@ -0,0 +1,30 @@
+-- Create database
+CREATE DATABASE Tarea6;
+USE Tarea6;
+
+-- Creando tabla Cursos
+CREATE TABLE Cursos (
+	CursoID INT PRIMARY KEY AUTO_INCREMENT, 
+    Sigla VARCHAR(10) UNIQUE KEY, -- Clave candidata
+    Nombre VARCHAR(50) UNIQUE KEY, -- Clave candidata
+    Semestre VARCHAR(10),
+    Creditos INT
+);
+
+-- Creando tabla Requisitos
+CREATE TABLE Requisitos (
+	RequisitoID INT PRIMARY KEY AUTO_INCREMENT,
+    CursoID INT, -- Clave foranea
+    RequisitoCursoID INT, -- Clave foranea
+    FOREIGN KEY (CursoID) REFERENCES Cursos(CursoID), 
+    FOREIGN KEY (RequisitoCursoID) REFERENCES Cursos(CursoID)
+);
+
+-- Creando tabla Descripciones
+CREATE TABLE Descripciones (
+	DescripcionID INT PRIMARY KEY AUTO_INCREMENT,
+    CursoID INT, -- Clave foranea
+    Descripcion VARCHAR(10000),
+    Dificultad ENUM('Fácil', 'Medio', 'Difícil'),
+    FOREIGN KEY (CursoID) REFERENCES Cursos(CursoID)
+);
